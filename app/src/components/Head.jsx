01b74551f0a1d24d4bffe012d2852dc34beb420f
thegameinsane <vlonpande@gmail.com>
2025-02-05 17:04:35 +0530

    minor changes

--- app/src/components/Head.jsx
+++ app/src/components/Head.jsx
@@ -5,22 +5,33 @@ const Head = () => {
     <>
       <nav className="flex items-center justify-between ">
         <div className="ml-5 hidden xl:block">
-          <div className="button border-2 text-xl font-merriweather"><a href="/" >Reservations</a></div>
-          <div className="button text-xl font-merriweather">
-            <a href="#menu" >Menu</a>
-          </div>
-          <div className="button text-xl font-merriweather">
-            <a href="#foot" >Contact</a>
-          </div>
+          <button className="button border-2 text-xl font-merriweather">
+            <a target="_main" href="https://www.zomato.com/">
+              Reservations
+            </a>
+          </button>
+          <button className="button text-xl font-merriweather">
+            <a href="#menu">Menu</a>
+          </button>
+          <button className="button text-xl font-merriweather">
+            <a href="#foot">Contact</a>
+          </button>
         </div>
-        <h1 className="hidden leading-40 sm:block sm:text-5xl lg:text-6xl pl-4 text-white font-playwrite font-bold select-none">
+        <div>
           <img
             src={engine}
             alt="engine"
             className="hidden md:w-60 xl:inline invert-100 h-50 bg-cover"
           />
-          The Great Indian Express
-        </h1>
+          <h1 className="hidden leading-40 sm:inline sm:text-5xl lg:text-6xl pl-4 text-white font-playwrite font-bold select-none">
+            The Great Indian Express
+          </h1>
+        </div>
+        <div className="xl:hidden h-10 w-10 m-4">
+          <hr className="text-white mb-3 border-2" />
+          <hr className="text-white mb-3 border-2" />
+          <hr className="text-white mb-3 border-2" />
+        </div>
       </nav>
     </>
   );
--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <>
       <div className="relative">
-        <div className="bg-[url(assets/hero.jpg)] bg-cover h-190"></div>
+        <div className="bg-[url(assets/hero.jpg)] bg-cover h-190 bg-center"></div>
         <div className="w-full h-full bg-linear-to-t from-black via-white/0 to-black absolute top-0 left-0"></div>
-        <div className="top-[40%] absolute w-full flex justify-center"><h1 className="text-white font-merriweather text-8xl font-semibold">Taste Authentic.</h1></div>
+        <div className="top-[40%] absolute w-full flex justify-center"><h1 className="text-white font-merriweather text-5xl sm:text-8xl font-semibold">Taste Authentic.</h1></div>
       </div>
     </>
   );
