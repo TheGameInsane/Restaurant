a357bc94b7ae15916cb1b0d989cba561da1e116b
thegameinsane <vlonpande@gmail.com>
2025-02-07 20:20:55 +0530

    small

--- app/src/components/Head.jsx
+++ app/src/components/Head.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import engine from "../assets/engine.svg";
 
 const Head = () => {
+  const [hamBurHide, setHamBurHide] = useState(false)
+
+  const hamburger = () => {
+    setHamBurHide(true)
+  }
+
+  // useEffect(() => {  
+  //   return () => {
+  //     second
+  //   }
+  // }, [hamBurHide])
+  
+
   return (
     <>
       <nav className="flex items-center justify-between ">
@@ -21,17 +35,17 @@ const Head = () => {
           <img
             src={engine}
             alt="engine"
-            className="hidden md:w-60 xl:inline invert-100 h-50 bg-cover"
+            className="w-40 md:w-60 xl:inline invert-100 h-50 bg-cover"
           />
           <h1 className="hidden leading-40 sm:inline sm:text-5xl lg:text-6xl pl-4 text-white font-playwrite font-bold select-none">
             The Great Indian Express
           </h1>
         </div>
-        <div className="xl:hidden h-10 w-10 m-4">
+        {/* <div onClick={hamburger} className="xl:hidden h-10 w-10 m-4 hover:cursor-pointer active:bg-white">
           <hr className="text-white mb-3 border-2" />
           <hr className="text-white mb-3 border-2" />
           <hr className="text-white mb-3 border-2" />
-        </div>
+        </div> */}
       </nav>
     </>
   );
